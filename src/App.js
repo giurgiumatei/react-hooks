52fa80e160e5269c78f54047d517f7142f05828c
DESKTOP-3L3V23H\giurg <giurgiu.matei@yahoo.com>
2021-10-11 02:02:38 +0300

    useRef hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef } from 'react';
+
+// allows to create a mutable object that will keep the same reference between renders
+// like useState, but mutable objects won't trigger a re-render when the value changes!
+// is used when you need to grab an element from the DOM
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const myButton = useRef(null);
+
+  const clickIt = () => myButton.current.click();
+
+  return(
+    <button ref={myButton}>
+
+    </button>
   );
 }
 
