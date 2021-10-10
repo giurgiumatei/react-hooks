776fe179477ae3d5cdb8db30b68c8015571581db
DESKTOP-3L3V23H\giurg <giurgiu.matei@yahoo.com>
2021-10-11 01:43:33 +0300

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
+
+// useEffect hook takes a function as its argument
+// will run the function everytime the dom is updated
+// in order not to have infinite loops caused by the DOM constinously updating we add as a second argument an array of dependencies
+// if the array is empty then it has no dependencies and will only run once, when the component is initialized
+// you can add a return value (function) that will run when the component is removed from the UI
 function App() {
+  const [count, setCount] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    alert('hello side effect!')
+
+    return () => alert('goodbye component!')
+
+  });
+
+  useEffect(() => {
+    fetch('foo').then(() => setLoaded(true));
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={() => setCount(count + 1)} style={{ margin: "0 auto" }}>
+      {count}
+    </button>
   );
 }
 
