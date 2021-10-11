934ae34803057d0d59b8ecab96e97286378d7427
DESKTOP-3L3V23H\giurg <giurgiu.matei@yahoo.com>
2021-10-11 16:18:41 +0300

    useReducer + Redux pattern

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
+
+// a different way to manage state using the Redux pattern
+// instead of updating the state directly, you dispatch actions for the reducer function
+// the hook takes the reduction function as parameter and the initial state
+// the reduction function will compute the next state
+// the first value returned is the reactive state that you want to show in the UI
+// the second value returned is a function that can dispatch an action
+
+function reducer(state, action) {
+  switch  (action.type) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    default:
+      throw new Error();
+  }
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [state, dispatch] = useReducer(reducer, 0);
+  
+  return (<>
+    Count: {state}
+    <button onClick={() => dispatch({type: 'decrement'})}>-</button>
+    <button onClick={() => dispatch({type: 'increment'})}>+</button>
+  </>);
 }
 
 export default App;
